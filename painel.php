3d08656525eba4441fa077e5ce9c7e002f769257
JoaaoSebastian <31390369+JoaaoSebastian@users.noreply.github.com>
2018-10-25 11:19:08 -0200

    mudando o painel

--- painel.php
+++ painel.php
@@ -18,51 +18,54 @@ require 'logincheck.php';
         <?php 
         // Include da NavBar
         include 'navbar.php';?>
-        <header>Painel do Usuário - VivAsilo</header>>
+        <header>Painel do Usuário - VivAsilo</header>
         
-        <p>Bem-vindo ao seu painel, <?php echo $_SESSION['nm_usuario']; ?> | <a href="loginout.php">Sair</a>
+        <div>Bem-vindo ao seu painel, <?php echo $_SESSION['nm_usuario']; ?> | <a href="loginout.php">Sair</a></div>
         <br/><?php if($_SESSION['administrador'] != 1){?>
-                        Você pode <a onclick="document.getElementById('id02').style.display='block'"> Troque o seu E-mail</a>
-                        ou <a onclick="document.getElementById('id03').style.display='block'"> Troque a sua Senha</a></br>
+        </br>
+        <div>Alterações de Perfil:</div>
+    <center><a onclick="document.getElementById('id02').style.display='block'"> Editar e-mail</a></br>
+                        <a onclick="document.getElementById('id03').style.display='block'"> Editar senha</a></br>
                         <?php if(maintainerch($_SESSION['id_usuario']) != true){?>
                             <p> Aguardando validação da conta.</p>
                         <?php } else { ?>
-                            Agora você pode <a onclick="document.getElementById('id04').style.display='block'"> Troque o seu número de Telefone</a>
-                            ou <a onclick="document.getElementById('id05').style.display='block'"> Troque a sua Foto</a>
+                            <a onclick="document.getElementById('id04').style.display='block'"> Editar número de telefone</a></br>
+                            <a onclick="document.getElementById('id05').style.display='block'"> Trocar foto</a></center>
                         <?php }
                     } ?>
-        </p>
+        
             <div id="id02" class="modal"> 
                 <form action="emailchange.php" method="post" class="modal-content animate">
                     <div class="imgcontainer">
                         <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
                     </div>
-                    <div class="container">
+                    
                         <label for="email"><b>Email:</b></label>
                         <input type="email" placeholder="Digite o seu novo e-mail" name="email" id="email" required>
 
                         <button type="submit">Trocar E-mail</button>
                         <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancelar</button>
-                    </div>
+                    
                 </form>
             </div><div id="id03" class="modal"> 
                 <form action="passwordchange.php" method="post" class="modal-content animate">
                     <div class="imgcontainer">
                         <span onclick="document.getElementById('id03').style.display='none'" class="close" title="Close Modal">&times;</span>
                     </div>
-                    <div class="container">
-                        <label for="senha"><b>Antiga Senha:</b></label>
-                        <input type="password" placeholder="Digite a sua Antiga Senha" name="senha" id="senha" required>
+                    
+                        <label for="senha"><b>Antiga senha:</b></label>
+                        <input type="password" placeholder="Digite a sua antiga senha" name="senha" id="senha" size="10" required>
 
-                        <label for="novasenha"><b>Nova Senha:</b></label>
-                        <input type="password" placeholder="Digite a sua Nova Senha" name="novasenha" id="novasenha" required>
+                        <label for="novasenha"><b>Nova senha:</b></label>
+                        <input type="password" placeholder="Digite a sua nova senha" name="novasenha" id="novasenha" required>
                         
-                        <label for="confsenha"><b>Confirmar Nova Senha::</b></label>
-                        <input type="password" placeholder="Confirme a Sua Nova Senha" name="confsenha" id="confsenha" required>
-
-                        <button type="submit">Trocar Senha</button>
+                        <label for="confsenha"><b>Confirmar nova senha:</b></label>
+                        <input type="password" placeholder="Confirme a sua nova senha" name="confsenha" id="confsenha" required>
+                        </br>
+                        </br>
+                        <button type="submit">Confirmar</button>
                         <button type="button" onclick="document.getElementById('id03').style.display='none'" class="cancelbtn">Cancelar</button>
-                    </div>
+                    
                 </form>
             </div>
     <script>
